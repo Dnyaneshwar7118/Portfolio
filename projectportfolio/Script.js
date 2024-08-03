c6597b089aa958d658b268e3b602c4a39f76bc8b
Dnyanu <dnyanumarathe436@gmail.com>
2024-08-03 19:20:13 +0530

    Create-Portfolio-using-Html,Css with respnsive

---
+++ projectportfolio/Script.js
@@ -0,0 +1,336 @@
+function headerFun()
+{
+	// document.getElementById("Headerfunction").style.background="#111827";
+	document.getElementById("Headerfunction1").style.background="#111827";
+	document.getElementById("Headerfunction").style.color="white";
+	document.getElementById("Button1").style.background="#111827";
+	document.getElementById("Button1").style.color="#99f6e4";
+	document.getElementById("Button2").style.background="#111827";
+	document.getElementById("Button2").style.color="#2dd4bf";
+	document.getElementById("Button").style.background="#111827";
+	document.getElementById("Button").style.color="#ffffff";
+	document.getElementById("Button").style.display="none";
+	document.getElementById("Button3").style.background="#111827";
+	document.getElementById("Button3").style.color="white";
+	document.getElementById("Button3").style.display="block";
+	
+
+	// Hover Buttton
+	const button = document.querySelector('button');
+	button.addEventListener('mouseover', () => {
+		button.style.backgroundColor = '#374151';
+	});
+	button.addEventListener('mouseout', () => {
+		button.style.backgroundColor = '#111827';
+	});
+
+	const Button2 = document.getElementById('Button2');
+	Button2.addEventListener('mouseover', () => {
+		Button2.style.backgroundColor = '#374151';
+	});
+	Button2.addEventListener('mouseout', () => {
+		Button2.style.backgroundColor = '#111827';
+	});
+
+	
+
+	document.getElementById("active1").style.color="#2dd4bf";
+	document.getElementById("active2").style.color="#2dd4bf";
+	document.getElementById("active3").style.color="#2dd4bf";
+
+
+	document.getElementById("headingName").style.background = "linear-gradient(170deg,#6ee7b7,#3b82f6,#9333ea) ";
+    document.getElementById("headingName").style.webkitTextFillColor = "transparent";
+    document.getElementById("headingName").style.webkitBackgroundClip = "text";
+
+	document.getElementById("headingName2").style.background = "linear-gradient(170deg,#6ee7b7,#3b82f6,#9333ea) ";
+    document.getElementById("headingName2").style.webkitTextFillColor = "transparent";
+    document.getElementById("headingName2").style.webkitBackgroundClip = "text";
+
+	document.getElementById("middleSection").style.fill="black";
+	document.getElementById("fill-current").style.fill="#111827";
+
+	document.getElementById("middleSection").style.background="black";
+	document.getElementById("middleSection1").style.background="black";
+
+	document.getElementById("rightFun").style.background="#111827";
+	document.getElementById("rightFun2").style.background="#111827";
+	
+	document.getElementById("leftFun").style.background="#111827";
+	document.getElementById("leftFun2").style.background="#111827";
+
+	document.getElementById("experianceWrapperFun").style.background="#111827";
+
+	document.getElementById("footerFun").style.background="black";
+
+	document.getElementById("img1").style.color="#2dd4bf";
+	document.getElementById("img2").style.color="#2dd4bf";
+	document.getElementById("img3").style.color="#2dd4bf";
+	
+	document.getElementById("arr1").style.color="black";
+	document.getElementById("arr1").style.backgroundColor="white";
+
+	document.getElementById("arr2").style.color="black";
+	document.getElementById("arr2").style.backgroundColor="white";
+
+	document.getElementById("arr3").style.color="black";
+	document.getElementById("arr3").style.backgroundColor="white";
+
+	document.getElementById("arr4").style.color="black";
+	document.getElementById("arr4").style.backgroundColor="white";
+
+	document.getElementById("arr5").style.color="black";
+	document.getElementById("arr5").style.backgroundColor="white";
+
+	// second Arrow
+
+	document.getElementById("arr6").style.color="black";
+	document.getElementById("arr6").style.backgroundColor="white";
+
+	document.getElementById("arr7").style.color="black";
+	document.getElementById("arr7").style.backgroundColor="white";
+	
+	document.getElementById("arro1").style.color="black";
+	document.getElementById("arro1").style.backgroundColor="white";
+
+	document.getElementById("arro2").style.color="black";
+	document.getElementById("arro2").style.backgroundColor="white";
+
+	document.getElementById("arro3").style.color="black";
+	document.getElementById("arro3").style.backgroundColor="white";
+
+	document.getElementById("arro4").style.color="black";
+	document.getElementById("arro4").style.backgroundColor="white";
+
+	document.getElementById("arro5").style.color="black";
+	document.getElementById("arro5").style.backgroundColor="white";
+
+	// Third Arrow
+
+	document.getElementById("arrow1").style.color="black";
+	document.getElementById("arrow1").style.backgroundColor="white";
+
+	document.getElementById("arrow2").style.color="black";
+	document.getElementById("arrow2").style.backgroundColor="white";
+
+	
+	document.getElementById("arrow3").style.color="black";
+	document.getElementById("arrow3").style.backgroundColor="white";
+
+	// Fourth Arrow
+
+	document.getElementById("rightarrow1").style.color="black";
+	document.getElementById("rightarrow1").style.backgroundColor="white";
+
+	document.getElementById("rightarrow2").style.color="black";
+	document.getElementById("rightarrow2").style.backgroundColor="white";
+
+	document.getElementById("rightarrow3").style.color="black";
+	document.getElementById("rightarrow3").style.backgroundColor="white";
+
+	document.getElementById("rightarrow4").style.color="black";
+	document.getElementById("rightarrow4").style.backgroundColor="white";
+
+	// Paragraph 
+
+	document.getElementById("paragraph1").style.color="#676c75";
+	document.getElementById("paragraph2").style.color="#676c75";
+	document.getElementById("paragraph3").style.color="#676c75";
+	document.getElementById("paragraph4").style.color="#676c75";
+	document.getElementById("paragraph5").style.color="#676c75";
+	document.getElementById("paragraph6").style.color="#676c75";
+	document.getElementById("paragraph7").style.color="#676c75";
+	document.getElementById("paragraph8").style.color="#676c75";
+	document.getElementById("paragraph9").style.color="#676c75";
+	document.getElementById("paragraph10").style.color="#676c75";
+	document.getElementById("paragraph11").style.color="#676c75";
+	document.getElementById("paragraph12").style.color="#676c75";
+
+	// Developer 1
+	document.getElementById("developer1").style.color="#676c75";
+	document.getElementById("developer2").style.color="#676c75";
+	document.getElementById("developer3").style.color="#676c75";
+
+	document.getElementById("Present1").style.color="#676c75";
+	document.getElementById("Present2").style.color="#676c75";
+	document.getElementById("Present3").style.color="#676c75";
+
+	// PlayButton Svg
+	document.getElementById("play_arrow1").style.color="#2dd4bf";
+	document.getElementById("play_arrow2").style.color="#2dd4bf";
+	document.getElementById("play_arrow3").style.color="#2dd4bf";
+	document.getElementById("play_arrow4").style.color="#2dd4bf";
+	document.getElementById("play_arrow5").style.color="#2dd4bf";
+	document.getElementById("play_arrow6").style.color="#2dd4bf";
+	document.getElementById("play_arrow7").style.color="#2dd4bf";
+	document.getElementById("play_arrow8").style.color="#2dd4bf";
+	document.getElementById("play_arrow9").style.color="#2dd4bf";
+	document.getElementById("play_arrow10").style.color="#2dd4bf";
+	document.getElementById("play_arrow11").style.color="#2dd4bf";
+	document.getElementById("play_arrow12").style.color="#2dd4bf";
+
+}
+
+function header2Fun() {
+	document.getElementById("Headerfunction1").style.background="#cffafe";
+	document.getElementById("Headerfunction").style.color="black";
+	document.getElementById("Button1").style.background="#cffafe";
+	document.getElementById("Button1").style.color="black";
+	document.getElementById("Button2").style.background="#cffafe";
+	document.getElementById("Button2").style.color="#4b5563";
+	document.getElementById("Button").style.background="#cffafe";
+	document.getElementById("Button").style.color="black";
+	document.getElementById("Button").style.display="block";
+	document.getElementById("Button3").style.background="#cffafe";
+	document.getElementById("Button3").style.color="black";
+	document.getElementById("Button3").style.display="none";
+
+
+	// Hower Button
+	const button = document.querySelector('button');
+	button.addEventListener('mouseover', () => {
+		button.style.backgroundColor = 'white';
+	});
+	button.addEventListener('mouseout', () => {
+		button.style.backgroundColor = '#cffafe';
+	});
+
+	const Button2 = document.getElementById('Button2');
+	Button2.addEventListener('mouseover', () => {
+		Button2.style.backgroundColor = 'white';
+	});
+	Button2.addEventListener('mouseout', () => {
+		Button2.style.backgroundColor = '#cffafe';
+	});
+
+	// Heading Name
+	document.getElementById("headingName").style.background = "black";
+    document.getElementById("headingName").style.webkitTextFillColor = "transparent";
+    document.getElementById("headingName").style.webkitBackgroundClip = "text";
+
+	document.getElementById("headingName2").style.background = "black";
+    document.getElementById("headingName2").style.webkitTextFillColor = "transparent";
+    document.getElementById("headingName2").style.webkitBackgroundClip = "text";
+
+	// Middle Section 
+	document.getElementById("middleSection").style.fill="white";
+	document.getElementById("fill-current").style.fill="#cffafe";
+
+	document.getElementById("middleSection").style.background="white";
+	document.getElementById("middleSection1").style.background="white";
+
+	document.getElementById("rightFun").style.background="white";
+	document.getElementById("rightFun2").style.background="white";
+	
+	document.getElementById("leftFun").style.background="white";
+	document.getElementById("leftFun2").style.background="white";
+
+	document.getElementById("img1").style.color="#22d3ee";
+	document.getElementById("img2").style.color="#22d3ee";
+	document.getElementById("img3").style.color="#22d3ee";
+
+	document.getElementById("experianceWrapperFun").style.background="#cffafe";
+	document.getElementById("footerFun").style.background="white";
+
+	// For Arrow
+
+	document.getElementById("arr1").style.color="white";
+	document.getElementById("arr1").style.backgroundColor="black";
+
+	document.getElementById("arr2").style.color="white";
+	document.getElementById("arr2").style.backgroundColor="black";
+
+	document.getElementById("arr3").style.color="white";
+	document.getElementById("arr3").style.backgroundColor="black";
+
+	document.getElementById("arr4").style.color="white";
+	document.getElementById("arr4").style.backgroundColor="black";
+
+	document.getElementById("arr5").style.color="white";
+	document.getElementById("arr5").style.backgroundColor="black";
+
+	// For Second Arrow
+	document.getElementById("arr6").style.color="white";
+	document.getElementById("arr6").style.backgroundColor="black";
+
+	document.getElementById("arr7").style.color="white";
+	document.getElementById("arr7").style.backgroundColor="black";
+	
+	document.getElementById("arro1").style.color="white";
+	document.getElementById("arro1").style.backgroundColor="black";
+
+	document.getElementById("arro2").style.color="white";
+	document.getElementById("arro2").style.backgroundColor="black";
+
+	document.getElementById("arro3").style.color="white";
+	document.getElementById("arro3").style.backgroundColor="black";
+
+	document.getElementById("arro4").style.color="white";
+	document.getElementById("arro4").style.backgroundColor="black";
+
+	document.getElementById("arro5").style.color="white";
+	document.getElementById("arro5").style.backgroundColor="black";
+
+	// For Arrow Third
+	document.getElementById("arrow1").style.color="white";
+	document.getElementById("arrow1").style.backgroundColor="black";
+
+	document.getElementById("arrow2").style.color="white";
+	document.getElementById("arrow2").style.backgroundColor="black";
+
+	
+	document.getElementById("arrow3").style.color="white";
+	document.getElementById("arrow3").style.backgroundColor="black";
+
+	// For Arrow Four
+
+	document.getElementById("rightarrow1").style.color="white";
+	document.getElementById("rightarrow1").style.backgroundColor="black";
+
+	document.getElementById("rightarrow2").style.color="white";
+	document.getElementById("rightarrow2").style.backgroundColor="black";
+
+	document.getElementById("rightarrow3").style.color="white";
+	document.getElementById("rightarrow3").style.backgroundColor="black";
+
+	document.getElementById("rightarrow4").style.color="white";
+	document.getElementById("rightarrow4").style.backgroundColor="black";
+
+	// For Paragraph
+	document.getElementById("paragraph1").style.color="black";
+	document.getElementById("paragraph2").style.color="black";
+	document.getElementById("paragraph3").style.color="black";
+	document.getElementById("paragraph4").style.color="black";
+	document.getElementById("paragraph5").style.color="black";
+	document.getElementById("paragraph6").style.color="black";
+	document.getElementById("paragraph7").style.color="black";
+	document.getElementById("paragraph8").style.color="black";
+	document.getElementById("paragraph9").style.color="black";
+	document.getElementById("paragraph10").style.color="black";
+	document.getElementById("paragraph11").style.color="black";
+	document.getElementById("paragraph12").style.color="black";
+
+	// Developer 1
+	document.getElementById("developer1").style.color="black";
+	document.getElementById("developer2").style.color="black";
+	document.getElementById("developer3").style.color="black";
+
+	document.getElementById("Present1").style.color="black";
+	document.getElementById("Present2").style.color="black";
+	document.getElementById("Present3").style.color="black";
+
+	document.getElementById("play_arrow1").style.color="#115e59";
+	document.getElementById("play_arrow2").style.color="#115e59";
+	document.getElementById("play_arrow3").style.color="#115e59";
+	document.getElementById("play_arrow4").style.color="#115e59";
+	document.getElementById("play_arrow5").style.color="#115e59";
+	document.getElementById("play_arrow6").style.color="#115e59";
+	document.getElementById("play_arrow7").style.color="#115e59";
+	document.getElementById("play_arrow8").style.color="#115e59";
+	document.getElementById("play_arrow9").style.color="#115e59";
+	document.getElementById("play_arrow10").style.color="#115e59";
+	document.getElementById("play_arrow11").style.color="#115e59";
+	document.getElementById("play_arrow12").style.color="#115e59";
+
+}
+
